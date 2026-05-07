cdbfe8c6b6f914ae33eb5b8f7264811e0cf3e2ed
Kauê Schaefer de Andrade <128431286+kauesandrade@users.noreply.github.com>
2026-05-06 21:05:53 -0300

    Tirando os default dos reads

--- DTOs/MedicoReadDTO.cs
+++ DTOs/MedicoReadDTO.cs
@@ -3,8 +3,8 @@ namespace ApiClinica.DTOs;
 public class MedicoReadDTO
 {
     public int Id { get; set; }
-    public string Nome { get; set; } = default!;
-    public string Email { get; set; } = default!;
-    public string Telefone { get; set; } = default!;
-    public string CRM { get; set; } = default!;
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
+    public string CRM { get; set; }
 }
--- DTOs/PacienteReadDTO.cs
+++ DTOs/PacienteReadDTO.cs
@@ -3,8 +3,8 @@ namespace ApiClinica.DTOs;
 public class PacienteReadDTO
 {
     public int Id { get; set; }
-    public string Nome { get; set; } = default!;
-    public string Email { get; set; } = default!;
-    public string Telefone { get; set; } = default!;
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Telefone { get; set; }
     public DateOnly DataNasc { get; set; }
 }
